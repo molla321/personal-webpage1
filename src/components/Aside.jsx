bef669b47cd36317182c1cb1c25f5b0640d5bd52
molla321 <145046924+molla321@users.noreply.github.com>
2024-05-29 03:53:46 +0600

    Hi There

---
+++ src/components/Aside.jsx
@@ -0,0 +1,116 @@
+import { GiCalendar, GiClawSlashes, GiMailbox, GiMayanPyramid, GiPhone } from "react-icons/gi";
+
+const Aside = () => {
+  return (
+    <aside className="sidebar">
+        <div className="sidebar-info">
+          <figure className="avatar-box">
+            <img
+              src={"/images/rabbi.jpg"}
+              alt="Richard hanrick"
+              width="80"
+            />
+          </figure>
+
+          <div className="info-content">
+            <h1 className="name" title="Richard hanrick">
+              Muhammad Rabbi
+            </h1>
+
+            <p className="title">Web developer</p>
+          </div>
+
+          <button className="info_more-btn" data-sidebar-btn>
+            <span>Show Contacts</span>
+
+            <GiClawSlashes />
+          </button>
+        </div>
+
+        <div className="sidebar-info_more">
+          <div className="separator"></div>
+
+          <ul className="contacts-list">
+            <li className="contact-item">
+              <div className="icon-box">
+                <GiMailbox/>
+                
+              </div>
+
+              <div className="contact-info">
+                <p className="contact-title">Email</p>
+
+                <a href="mailto:richard@example.com" className="contact-link">
+                  rabbi157@gmail.com
+                </a>
+              </div>
+            </li>
+
+            <li className="contact-item">
+              <div className="icon-box">
+                <GiPhone/>
+              </div>
+
+              <div className="contact-info">
+                <p className="contact-title">Phone</p>
+
+                <a href="tel:+12133522795" className="contact-link">
+                  +880 192414-0620
+                </a>
+              </div>
+            </li>
+
+            <li className="contact-item">
+              <div className="icon-box">
+                <GiCalendar/>
+               
+              </div>
+
+              <div className="contact-info">
+                <p className="contact-title">Birthday</p>
+
+                <time dateTime="1982-06-23">Sep 7, 2004</time>
+              </div>
+            </li>
+
+            <li className="contact-item">
+              <div className="icon-box">
+                <GiMayanPyramid/>
+                <ion-icon name="location-outline"></ion-icon>
+              </div>
+
+              <div className="contact-info">
+                <p className="contact-title">Location</p>
+
+                <address>Narayangonj 251</address>
+              </div>
+            </li>
+          </ul>
+
+          <div className="separator"></div>
+
+          <ul className="social-list">
+            <li className="social-item">
+              <a href="#" className="social-link">
+                <ion-icon name="logo-facebook"></ion-icon>
+              </a>
+            </li>
+
+            <li className="social-item">
+              <a href="#" className="social-link">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </a>
+            </li>
+
+            <li className="social-item">
+              <a href="#" className="social-link">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+  )
+}
+
+export default Aside
